dd20387492af4186cea1f5e2a6612c80ee5a643b
tatianakharitonovich <k.tanya@tut.by>
2021-08-02 15:25:55 +0300

    Create AlertState.js

---
+++ src/context/alert/AlertState.js
@@ -0,0 +1,26 @@
+import {useReducer} from 'react';
+import {AlertContext} from './alertContext';
+import {alertReducer} from './alertReducer';
+import {SHOW_ALERT, HIDE_ALERT} from '../types'
+
+function AlertState ({children}) {
+	const [state, dispatch] = useReducer(alertReducer, null);
+
+	const hide = ()=> dispatch({type: HIDE_ALERT});
+	const show = (text, type = 'secondary') => {
+		dispatch({
+			type: SHOW_ALERT,
+			payload: {text, type}
+		})
+	}
+
+	return (
+		<AlertContext.Provider value = {{
+			hide, show, alert: state
+		}}>
+			{children}
+		</AlertContext.Provider>
+	)
+}
+
+export default AlertState;
