3ae6b319641a6e288c1d3d40d4dfe1d166e94c39
tatianakharitonovich <k.tanya@tut.by>
2021-08-02 15:25:26 +0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import Navbar from './components/Navbar';
+import Alert from './components/Alert';
+import AlertState from './context/alert/AlertState';
+import GithubState from './context/github/GithubState';
+import Home from './pages/Home';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+
+function App() {
+  return (
+    <GithubState>
+      <AlertState>
+        <BrowserRouter>
+          <Navbar />
+          <div className='container pt-4'>
+            <Alert />
+            <Switch>
+              <Route path='/'exact component={Home} />
+              <Route path='/about' component={About} />
+              <Route path='/profile/:name' component={Profile} />
+            </Switch>
+          </div>
+        </BrowserRouter>
+      </AlertState>
+    </GithubState>
+  );
+}
+
+export default App;
