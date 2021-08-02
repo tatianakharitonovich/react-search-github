ad83ab41512f6de835d5dde29b070b78d8a8a960
tatianakharitonovich <k.tanya@tut.by>
2021-08-02 15:26:40 +0300

    Create About.js

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+function About () {
+	return(
+		<div className="d-grid gap-3">
+  			<div className="p-2 bg-light border">
+  				<h1 className = 'display-4'>Информация</h1>
+  				<p className = 'lead'>Версия приложения: <strong>1.0.0</strong></p>
+  			</div>
+ 		</div>
+	)
+}
+
+export default About;
